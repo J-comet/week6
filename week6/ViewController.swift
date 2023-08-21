e1be382f30d0ef791922a6975dd5d6d2eb9274e5
hyeseong <hyeseong7848@gmail.com>
2023-08-21 11:57:06 +0900

    Initial Commit

---
+++ week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by 장혜성 on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
